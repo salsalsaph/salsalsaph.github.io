61514a2f2f9121631ed757d1cc415b07a4820d3e
salsalsaph <42301190+salsalsaph@users.noreply.github.com>
2018-09-04 01:42:23 +0800

    try fullscreen

--- room-2-360/data.js
+++ room-2-360/data.js
@@ -81,7 +81,7 @@ var APP_DATA = {
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "fullscreenButton": true,
     "viewControlButtons": true
   }
 };
