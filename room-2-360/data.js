6de1ac936a9ec50169eb987da6d5efefb1cb57d2
salsalsaph <42301190+salsalsaph@users.noreply.github.com>
2018-09-04 01:17:30 +0800

    360

---
+++ room-2-360/data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010321_20180809151729",
+      "name": "R0010321_20180809151729",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.1049080049924402,
+        "pitch": 0.09273684317586373,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.71584361913321,
+          "pitch": 0.052189892601195,
+          "rotation": 14.922565104551524,
+          "target": "1-r0010323_20180809151920"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7093418204591266,
+          "pitch": 0.19626750183296338,
+          "title": "Kitchen",
+          "text": "the kitchen experience<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010323_20180809151920",
+      "name": "R0010323_20180809151920",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.4889594786578755,
+        "pitch": 0.014349508706848368,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "room 2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
